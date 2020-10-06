1128c118d51bcb2a62a5dd34313840668b15b5cd
TrifShterev <tshterev@yahoo.com>
2020-10-06 22:29:30 +0300

    Add project files.

---
+++ Car/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CarManufacturer
+
+{
+  public  class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+
+        public Car()
+        {
+            this.make = "VW";
+            this.model = "Golf";
+            this.year = 2025;
+            this.fuelQuantity = 200;
+            this.fuelConsumption = 10;
+        }
+        public Car(string make, string model, int year)
+            : this()
+        {
+            this.Model = model;
+            this.Make = make;
+            this.Year = year;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+            : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public string Make { get { return this.make; } set { this.make = value; } }
+        public string Model { get { return this.model; } set { this.model = value; } }
+        public int Year { get {return this.year; } set { this.year = value; } }
+        public double FuelQuantity { get { return this.fuelQuantity; } set { this.fuelQuantity = value; } }
+        public double FuelConsumption { get { return this.fuelConsumption; } set { this.fuelConsumption = value; } }
+  
+        public void Drive(double distance)
+        {
+            bool isGoodToGo = (fuelQuantity - (distance * fuelConsumption)) >= 0;
+            if (isGoodToGo)
+            {
+                fuelQuantity = fuelQuantity - (distance * fuelConsumption);
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}L";
+
+        }
+    }
+}
