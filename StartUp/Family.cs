508803c26fec96367dfb4fe0eeb87161fca6a878
TrifShterev <tshterev@yahoo.com>
2020-10-08 18:20:03 +0300

    Defining classes added

---
+++ StartUp/Family.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace DefiningClasses
+{
+  public  class Family
+    {
+        public Family()
+        {
+            this.FamilyMembers = new List<Person>();
+        }
+        public List<Person> FamilyMembers { get;set; }
+
+        public void AddMember(Person member) {
+            
+
+            FamilyMembers.Add(member);
+            
+        }
+        public Person GetOldestMember()
+
+        {
+
+            var oldestPerson = FamilyMembers.OrderByDescending(x=>x.Age).FirstOrDefault();
+
+            return oldestPerson;
+
+        }
+    }
+}
---
+++ StartUp/StartUp .cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace DefiningClasses
+{
+  public  class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var family = new Family();
+            for (int i = 0; i < n; i++)
+            {
+                string[] newPerson = Console.ReadLine().Split();
+                int age = int.Parse(newPerson[1]);
+
+                var newFamilyMember = new Person(newPerson[0], age);
+                family.AddMember(newFamilyMember);
+
+            }
+            var oldestMember = family.GetOldestMember();
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+           
+            
+        }
+    }
+}
